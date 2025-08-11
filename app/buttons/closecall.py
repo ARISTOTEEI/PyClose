357281c1f683e7dc710e977a3047eecbafe69316
ARISTOTEEI <Footbolkross3@gmail.com>
2025-08-11 15:29:45 +0500

    fix: add action if not closemod,not creator

--- app/buttons/closecall.py
+++ app/buttons/closecall.py
@@ -14,28 +14,34 @@ class CloseCallButton(Cog):
     @commands.Cog.listener()
     async def on_button_click(self,inter:dis.MessageInteraction):
         if inter.component.custom_id.startswith("closecall"):
-            await inter.response.defer(with_message=True,ephemeral=True)
-            raw_data = inter.component.custom_id.split(".")
-            close_id = int(raw_data[-1])
-            close = await self.bot.clm.getCloseById(close_id)
-            time = datetime.datetime.fromtimestamp(close.lastcall if close.lastcall != None else 0)
-            now = datetime.datetime.now()
-            timeout = datetime.timedelta(minutes=5)
-            if (now - time).total_seconds() >= timeout.total_seconds():
-                members = await self.bot.clm.get_members(close_id)
-                mentions = ""
-                poses = [2,2,2,2,2]
-                for member in members:
-                    poses[member.pos-1] -= 1
-                for index,pos in enumerate(poses,1):
-                    mentions += f" +{pos} {self.bot.settings.line[str(index)]}"
-                role = inter.guild.get_role(self.bot.settings.roles.closenotify)
-                await inter.guild.get_channel(close.managechannel).send(f"{role.mention} {mentions}")
-                await inter.edit_original_message('## Вы успешно позвали людей на клоз.')
-                update = CloseUpdateSchema(lastcall=int(now.timestamp()),message=close.message)
-                await self.bot.clm.update_close(close_id,update)
+            if inter.guild.get_role(self.bot.settings.roles.closemod) in inter.author.roles:
+                close = await self.bot.clm.getCloseById(close_id)
+                if inter.author.id == close.creator:
+                    await inter.response.defer(with_message=True,ephemeral=True)
+                    raw_data = inter.component.custom_id.split(".")
+                    close_id = int(raw_data[-1])
+                    time = datetime.datetime.fromtimestamp(close.lastcall if close.lastcall != None else 0)
+                    now = datetime.datetime.now()
+                    timeout = datetime.timedelta(minutes=5)
+                    if (now - time).total_seconds() >= timeout.total_seconds():
+                        members = await self.bot.clm.get_members(close_id)
+                        mentions = ""
+                        poses = [2,2,2,2,2]
+                        for member in members:
+                            poses[member.pos-1] -= 1
+                        for index,pos in enumerate(poses,1):
+                            mentions += f" +{pos} {self.bot.settings.line[str(index)]}"
+                        role = inter.guild.get_role(self.bot.settings.roles.closenotify)
+                        await inter.guild.get_channel(close.managechannel).send(f"{role.mention} {mentions}")
+                        await inter.edit_original_message('## Вы успешно позвали людей на клоз.')
+                        update = CloseUpdateSchema(lastcall=int(now.timestamp()),message=close.message)
+                        await self.bot.clm.update_close(close_id,update)
+                    else:
+                        await inter.edit_original_message("## Вы не можете звать людей на клоз чаще чем раз в 5 минут.")
             else:
-                await inter.edit_original_message("## Вы не можете звать людей на клоз чаще чем раз в 5 минут.")
+                await inter.response.send_message("Вы не создатель клоза",ephemeral=True)
+        else:
+            await inter.response.send_message("Вы не являетесь клозмодом",ephemeral=True)
 
 def setup(bot:CloseBot):
     bot.add_cog(CloseCallButton(bot))
