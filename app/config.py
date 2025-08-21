7240f53996cee641a5ab694db49cc5430114beef
ARISTOTEEI <Footbolkross3@gmail.com>
2025-08-21 12:01:50 +0500

    feat(all): for production

--- app/config.py
+++ app/config.py
@@ -2,23 +2,23 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import BaseModel, Field
 
 roles = {
-    "closemod": 1160124199902396458,
-    "closeban": 1160128387390636085,
-    "closenotify": 1160124206177079357
+    "closemod": 0,
+    "closeban": 0,
+    "closenotify": 0
 }
 
 channels = {
-    "log_channel": 13213,
-    "notification_channel": 1011561762417557576,
-    "win_channel": 1011561762417557576
+    "log_channel": 0,
+    "notification_channel": 0,
+    "win_channel": 0
 }
 
 emojis = {
-    "Knife": "<:freeiconsword842082:1405556977014407168>",
-    "Onion": "<:freeiconbow1885298:1405557001982967919>",
-    "Security": "<:freeiconshield3077175:1405557022908350654>",
-    "Conhand": "<:freeiconhand1534403:1405557053618913320>",
-    "Conhands": "<:freeiconheart3477186:1405557077807468585>",
+    "Knife": "",
+    "Onion": "",
+    "Security": "",
+    "Conhand": "",
+    "Conhands": "",
     "dark": "🌑",
     "light": "🌕"
 }
@@ -67,7 +67,7 @@ class Settings(BaseSettings):
     line: dict = Field(default=line)
 
     TOKEN: str = Field(
-        default="MTAwMjIyMTYxMTM4NDA2MjAyNA.GNmHdQ.qXaw-2BZfGS4ZbZxfFbenjP6GxjAiCaGNuUdGw")
+        default="TOKEN")
 
     @property
     def DATABASE_url_asyncpg(self):
